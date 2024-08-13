b002ea893f69e64e6d1b18c482f99f49a1545d0a
Nick <Nick_II2000@mail.ru>
2024-08-13 21:44:51 +0600

    28-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -13,9 +13,17 @@ import './heroesList.scss';
 // При клике на "крестик" идет удаление персонажа из общего состояния
 // Усложненная задача:
 // Удаление идет и с json файла при помощи метода DELETE
-// // -- "27-th commit" --
+// // -- "28-th commit" --
 const HeroesList = () => {
-    const { filteredHeroes, heroesLoadingStatus } = useSelector(state => state);
+    const filteredHeroes = useSelector(state => {
+        if (state.activeFilter === 'all') {
+            return state.heroes;
+        } else {
+            return state.heroes.filter(item => item.element === state.activeFilter);
+        }
+
+    });
+    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatusheroesLoadingStatus);
     const dispatch = useDispatch();
     const { request } = useHttp();
 
