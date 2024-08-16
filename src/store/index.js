06e751fdab1da65bfa617268c0cfc0e17673cbc5
Nick <Nick_II2000@mail.ru>
2024-08-16 20:20:28 +0600

    48-th commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 // import { createStore } from 'redux';
-import { legacy_createStore as createStore, combineReducers, compose } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+// const stringMiddleware = (store) => (dispatch) => (action) => {
+// const stringMiddleware = ({ despatch, getState }) => (dispatch) => (action) => {
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
+    }
+    return next(action);
+};
 
 const enhancer = (createStore) => (...args) => {
     const store = createStore(...args);
@@ -21,7 +32,8 @@ const enhancer = (createStore) => (...args) => {
 // const store = createStore(combineReducers({ heroes: heroes, filters: filters }),
 const store = createStore(
     combineReducers({ heroes, filters }),
-    compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
