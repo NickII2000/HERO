ad0a71eafa7a48164e8adbc9f323d8ccfa36eef5
Nick <Nick_II2000@mail.ru>
2024-08-25 22:56:04 +0600

    77-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -14,7 +14,7 @@ import './heroesList.scss';
 // При клике на "крестик" идет удаление персонажа из общего состояния
 // Усложненная задача:
 // Удаление идет и с json файла при помощи метода DELETE
-// // --- "76-th commit" ---
+// // --- "77-th commit" ---
 const HeroesList = () => {
 
     const filteredHeroesSelector = createSelector(
