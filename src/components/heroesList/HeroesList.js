6121ee53fe15716b4235104947e7e963fdc6e6b4
Nick <Nick_II2000@mail.ru>
2024-09-22 15:02:28 +0600

    112-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -10,7 +10,7 @@ import { heroDeleted } from './heroesSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 import './heroesList.scss';
-// // --- "111-th commit" ---
+// // --- "112-th commit" ---
 
 const HeroesList = () => {
     const filteredHeroesSelector = createSelector(
