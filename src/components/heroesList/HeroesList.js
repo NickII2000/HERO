1214a691ea18e1b4afdfe84dacc55634f4962975
Nick <Nick_II2000@mail.ru>
2024-11-06 22:01:58 +0600

    217-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -11,7 +11,7 @@ import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 import './heroesList.scss';
-// // ---  "216-th commit"  ---
+// // ---  "217-th commit"  ---
 
 const HeroesList = () => {
 
