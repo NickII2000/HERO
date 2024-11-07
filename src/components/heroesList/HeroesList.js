b56a02827a5aab10c3d09a3e5df14f465d0e20b3
Nick <Nick_II2000@mail.ru>
2024-11-07 22:17:04 +0600

    221-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -10,7 +10,7 @@ import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 import './heroesList.scss';
-// // ---  "220-th commit"  ---
+// // ---  "221-th commit"  ---
 
 const HeroesList = () => {
 
@@ -32,7 +32,7 @@ const HeroesList = () => {
     const { request } = useHttp();
 
     useEffect(() => {
-        dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
         // eslint-disable-next-line
     }, []);
 
