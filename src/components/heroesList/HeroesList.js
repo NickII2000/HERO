68d8b71f0020700f457111c49237fe299511d715
Nick <Nick_II2000@mail.ru>
2025-12-29 23:03:25 +0600

    565-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -13,7 +13,7 @@ import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from "../spinner/Spinner";
 
 import "./heroesList.scss";
-// // -----  "564-th commit"  -----
+// // -----  "565-th commit"  -----
 
 const HeroesList = () => {
 	const filteredHeroes = useSelector(filteredHeroesSelector);
