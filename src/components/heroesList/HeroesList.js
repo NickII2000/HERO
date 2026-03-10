1ea6006961812a45cd3698a517dc8a97a9bc5295
Nick <Nick_II2000@mail.ru>
2026-03-10 22:39:18 +0600

    616-th commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -13,7 +13,7 @@ import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from "../spinner/Spinner";
 
 import "./heroesList.scss";
-// // -----  "615-th commit"  -----
+// // -----  "616-th commit"  -----
 
 const HeroesList = () => {
 	const filteredHeroes = useSelector(filteredHeroesSelector);
